a741b8a85effb43a4746b142c6314f6924a128b9
Kenneth <matovken@gmail.com>
2015-09-24 22:22:07 +0300

    some refactor

--- whatapp/app/views.py
+++ whatapp/app/views.py
@@ -8,12 +8,12 @@ __author__ = 'kenneth'
 
 @csrf_exempt
 def handle_rapidpro(request):
-    # if request.method == 'POST':
-    text = request.GET.get('text')
-    fro = request.GET.get('to')
-    id = request.GET.get("id")
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        fro = request.POST.get('to')
+        id = request.POST.get("id")
 
-    m = Message.objects.create(direction=Message.OUTGOING, text=text, urn=fro.strip('+'), rapidpro_id=id)
-    tasks.push_out([m.pk])
-    return HttpResponse(status=200)
-    # return HttpResponse(status=401)
+        m = Message.objects.create(direction=Message.OUTGOING, text=text, urn=fro.strip('+'), rapidpro_id=id)
+        tasks.push_out.delay([m.pk])
+        return HttpResponse(status=200)
+    return HttpResponse(status=401)
