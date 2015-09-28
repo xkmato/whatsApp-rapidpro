a1e48f9f57b3bfe3026bfbe92f0f5ee88e4fb8f9
Kenneth <matovken@gmail.com>
2015-09-28 17:01:16 +0300

    give up with locking the tasks

--- whatapp/app/tasks.py
+++ whatapp/app/tasks.py
@@ -13,25 +13,18 @@ logger = get_task_logger(__name__)
 
 @task
 def push_out(limit=30):
-    """
-    Get all messages in Queue
-    """
-    r = get_redis_connection()
-    key = 'pcmmessage_in_queue'
-    if not r.get(key):
-        with r.lock(key, timeout=60*20):
-            messages = Message.objects.filter(direction=Message.OUTGOING, status=Message.QUEUED).order_by('created_on')
+    messages = Message.objects.filter(direction=Message.OUTGOING, status=Message.QUEUED).order_by('created_on')
 
-            # somebody already handled these messages, move on
-            if not messages:
-                return
-            for message in messages:
-                print "[%s] Processing message %s" % (str(datetime.now()), message.text)
-                msg = [(message.urn, message.text)]
-                y = YowsupSendStack(msg)
-                y.start()
-                message.status = Message.SENT
-                message.save()
+    # somebody already handled these messages, move on
+    if not messages:
+        return
+    for message in messages:
+        print "[%s] Processing message %s" % (str(datetime.now()), message.text)
+        msg = [(message.urn, message.text)]
+        y = YowsupSendStack(msg)
+        y.start()
+        message.status = Message.SENT
+        message.save()
 
 
 
