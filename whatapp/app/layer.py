640a27e8caea02a4cf4a2ca653c25707b3c33d6c
Kenneth <matovken@gmail.com>
2015-09-28 15:45:42 +0300

    adding timestamps to prints

--- whatapp/app/layer.py
+++ whatapp/app/layer.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 from whatapp.app import tasks
 from whatapp.app.models import Message
@@ -26,26 +27,27 @@ class EchoLayer(YowInterfaceLayer):
         self.toLower(entity.ack())
 
     def onTextMessage(self,messageProtocolEntity):
-        print("Received %s to %s" % (messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(False)))
+        print("[%s] Received %s to %s" % (str(datetime.now()), messageProtocolEntity.getBody(),
+                                          messageProtocolEntity.getFrom(False)))
         m = Message.receive(messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(False))
         tasks.push_to_rapidpro.delay([m])
 
     def onMediaMessage(self, messageProtocolEntity):
         # just print info
         if messageProtocolEntity.getMediaType() == "image":
-            print("Received image %s to %s" % (messageProtocolEntity.url, messageProtocolEntity.getFrom(False)))
+            print("[%s] Received image %s to %s" % (str(datetime.now()), messageProtocolEntity.url, messageProtocolEntity.getFrom(False)))
             m = Message.receive(messageProtocolEntity.url, messageProtocolEntity.getFrom(False))
             tasks.push_to_rapidpro.delay([m])
 
         elif messageProtocolEntity.getMediaType() == "location":
-            print("Received location (%s, %s) to %s" % (messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude(), messageProtocolEntity.getFrom(False)))
+            print("[%s] Received location (%s, %s) to %s" % (str(datetime.now()), messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude(), messageProtocolEntity.getFrom(False)))
             m = Message.receive("(%s, %s)" % (messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude()), messageProtocolEntity.getFrom(False))
             tasks.push_to_rapidpro.delay([m])
 
         elif messageProtocolEntity.getMediaType() == "vcard":
-            print("Received vcard (%s, %s) to %s" % (messageProtocolEntity.getName(), messageProtocolEntity.getCardData(), messageProtocolEntity.getFrom(False)))
+            print("[%s] Received vcard (%s, %s) to %s" % (str(datetime.now()), messageProtocolEntity.getName(), messageProtocolEntity.getCardData(), messageProtocolEntity.getFrom(False)))
             m = Message.receive("(%s, %s)" % (messageProtocolEntity.getName(), messageProtocolEntity.getCardData()), messageProtocolEntity.getFrom(False))
             tasks.push_to_rapidpro.delay([m])
 
         else:
-            print("Received unsupported message")
+            print("[%] Received unsupported message" % datetime.now())
