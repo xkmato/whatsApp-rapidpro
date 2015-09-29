e21bd0defe823504b5d720e38727d81223ffa55c
Kenneth <matovken@gmail.com>
2015-09-29 09:46:15 +0300

    send messages forever task

--- whatapp/app/tasks.py
+++ whatapp/app/tasks.py
@@ -11,22 +11,25 @@ __author__ = 'kenneth'
 logger = get_task_logger(__name__)
 
 
-@task
 def push_out(limit=30):
     messages = Message.objects.filter(direction=Message.OUTGOING, status=Message.QUEUED).order_by('created_on')
 
     # somebody already handled these messages, move on
     if not messages:
+        logger.info("No more messages, now taking a 10 second nap")
+        time.sleep(10)
         return
+
     messages.update(status=Message.LIMBO)
     for message in messages:
-        print "[%s] Processing message %s" % (str(datetime.now()), message.text)
+        logger.info("[%s] Processing message %s" % (str(datetime.now()), message.text))
         msg = [(message.urn, message.text)]
         y = YowsupSendStack(msg)
         y.start()
         message.status = Message.SENT
         message.save()
-
+        logger.info("Message sent, now taking a 2 seconds nap")
+        time.sleep(2)
 
 
 @task
@@ -35,3 +38,9 @@ def push_to_rapidpro(messages=None):
         messages = Message.objects.filter(direction=Message.INCOMING, status=Message.QUEUED).order_by('created_on')
     for message in messages:
         message.notify_rapidpro_received()
+
+
+@task
+def send_messages_out_forever():
+    while True:
+        push_out()
--- whatapp/app/views.py
+++ whatapp/app/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
+import json
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from whatapp.app import tasks
 from whatapp.app.models import Message
-from whatapp.app.utils import send_message_out
 
 __author__ = 'kenneth'
 
@@ -14,7 +13,6 @@ def handle_rapidpro(request):
         fro = request.POST.get('to')
         id = request.POST.get("id")
 
-        m = Message.objects.create(direction=Message.OUTGOING, text=text, urn=fro.strip('+'), rapidpro_id=id)
-        send_message_out(m)
+        Message.objects.create(direction=Message.OUTGOING, text=text, urn=fro.strip('+'), rapidpro_id=id)
         return HttpResponse(status=200)
-    return HttpResponse(status=401)
+    return JsonResponse(json.dumps({'error': 'Bad Request', 'message': 'This activity has been reported'}), status=401)
